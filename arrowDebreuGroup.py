5417827656d49ccfbfcd75a118d5f54f8323a373
Dimitri DUBOIS <dimitri.dubois@umontpellier.fr>
2019-06-30 15:37:33 +0200

    set the treatment

--- arrowDebreuGroup.py
+++ arrowDebreuGroup.py
@@ -29,6 +29,7 @@ class ADGroup(Group, Base):
     def __init__(self, le2msrv, group_id, players, num_sequence):
         Group.__init__(self, le2msrv, group_id, players)
         self.AD_sequence = num_sequence
+        self.AD_treatment = pms.TREATMENT
         self.current_period = None
 
     def new_period(self, period):
